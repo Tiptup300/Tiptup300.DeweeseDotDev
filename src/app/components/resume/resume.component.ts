88173bb7318e9bd0b1db90bf1ee70ad935f5b68e
mDeweeseCCD <73185526+mDeweeseCCD@users.noreply.github.com>
2021-07-22 15:30:59 -0400

    Implemented changing titles

--- src/app/components/resume/resume.component.ts
+++ src/app/components/resume/resume.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-resume',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResumeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private titleService:Title) { }
 
   ngOnInit(): void {
+    this.titleService.setTitle("Matthew Deweese - My Resume");
   }
 
 }
