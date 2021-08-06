de80717380f0acbac76d2e785b5f90ace64d974b
mDeweeseCCD <73185526+mDeweeseCCD@users.noreply.github.com>
2021-08-06 12:15:55 -0400

    implement styles for printing

--- src/app/site/header/header.component.ts
+++ src/app/site/header/header.component.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import { faEnvelope, faPhone, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import {
+  faEnvelope,
+  faPhone,
+  faMapMarkerAlt,
+  faPortrait,
+} from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
   faEnvelope = faEnvelope;
   faPhone = faPhone;
   faMapMarkerAlt = faMapMarkerAlt;
+  faPortrait = faPortrait;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
