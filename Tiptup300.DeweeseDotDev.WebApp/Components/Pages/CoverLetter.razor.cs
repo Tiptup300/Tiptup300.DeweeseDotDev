da3449624bd5b4cdc50ce236c18bd14913f253e5
Matthew Deweese <tiptup300@gmail.com>
2024-11-16 00:42:43 -0500

    Implemented CoverLetter & improved Resume Objective section
    - Implemented CoverLetter
    - Revamped objective section to be core qualities
    - Fixed nav animation
    - Removed Focus bug
    - Removed Unecessary Home

---
+++ Tiptup300.DeweeseDotDev.WebApp/Components/Pages/CoverLetter.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace Tiptup300.DeweeseDotDev.WebApp.Components.Pages
+{
+   public partial class CoverLetter : ComponentBase
+   {
+      private readonly IJSRuntime _jsRuntime;
+
+      public CoverLetter(IJSRuntime jsRuntime)
+      {
+         _jsRuntime = jsRuntime;
+      }
+
+      private string? businessName;
+      private string? businessAddressLine;
+      private string? businessCity;
+      private string? jobTitle;
+      private string? heardAbout;
+      private string? jobReferenceLink;
+
+      private void Print()
+      {
+         _jsRuntime.InvokeVoidAsync("window.print");
+      }
+
+   }
+}
