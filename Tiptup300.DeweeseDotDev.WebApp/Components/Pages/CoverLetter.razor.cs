ac7abdc67a15e10f528bc3376c828025ba139aa9
Matthew Deweese <tiptup300@gmail.com>
2024-11-27 00:01:34 -0500

    removed junk assets changed namespace to top level for coverletter

--- Tiptup300.DeweeseDotDev.WebApp/Components/Pages/CoverLetter.razor.cs
+++ Tiptup300.DeweeseDotDev.WebApp/Components/Pages/CoverLetter.razor.cs
@@ -1,28 +1,27 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 
-namespace Tiptup300.DeweeseDotDev.WebApp.Components.Pages
+namespace Tiptup300.DeweeseDotDev.WebApp.Components.Pages;
+
+public partial class CoverLetter : ComponentBase
 {
-   public partial class CoverLetter : ComponentBase
+   private readonly IJSRuntime _jsRuntime;
+
+   public CoverLetter(IJSRuntime jsRuntime)
    {
-      private readonly IJSRuntime _jsRuntime;
-
-      public CoverLetter(IJSRuntime jsRuntime)
-      {
-         _jsRuntime = jsRuntime;
-      }
-
-      private string? businessName;
-      private string? businessAddressLine;
-      private string? businessCity;
-      private string? jobTitle;
-      private string? heardAbout;
-      private string? jobReferenceLink;
-
-      private void Print()
-      {
-         _jsRuntime.InvokeVoidAsync("window.print").GetAwaiter().GetResult();
-      }
-
+      _jsRuntime = jsRuntime;
    }
+
+   private string? businessName;
+   private string? businessAddressLine;
+   private string? businessCity;
+   private string? jobTitle;
+   private string? heardAbout;
+   private string? jobReferenceLink;
+
+   private void Print()
+   {
+      _jsRuntime.InvokeVoidAsync("window.print").GetAwaiter().GetResult();
+   }
+
 }
