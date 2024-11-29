a1a6dc2321bdf216ac9a61d991a8fe0a879a46f6
Matthew Deweese <tiptup300@gmail.com>
2024-11-28 22:55:34 -0500

    -renamed pages to follow xPage.razor - moved code on components to the top - created portfolio gallery page - craeted portfolio project page

--- Tiptup300.DeweeseDotDev.Portfolio/Projects/Project.cs
+++ Tiptup300.DeweeseDotDev.Portfolio/Projects/Project.cs
@@ -9,4 +9,7 @@ public record Project
   IReadOnlyList<IFigure> Figures,
   IReadOnlyList<Link> Links,
   IReadOnlyList<string> Tags
-);
+)
+{
+   public ImageFigure? PrimaryFigure => Figures.FirstOrDefault(x => x is ImageFigure && x.IsPrimary) as ImageFigure;
+}
